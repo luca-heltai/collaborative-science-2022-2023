f98c6963ec95ee55ac7116c5e845d124e45ba2e0
fcaretti <federico.caretti@protonmail.com>
2022-11-28 22:03:38 +0100

    Training of network for testing implemented

---
+++ tests/fcaretti/utils/training.py
@@ -0,0 +1,20 @@
+def train(dataloader, model, loss_fn, optimizer,device):
+    size = len(dataloader.dataset)
+    model.train()
+    for batch, (X, y) in enumerate(dataloader):
+        X, y = X.to(device), y.to(device)
+
+        # Compute prediction error
+        pred = model(X)
+        loss = loss_fn(pred, y)
+
+        # Backpropagation
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        if batch % 100 == 0:
+            loss, current = loss.item(), batch * len(X)
+            print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+    return loss.item()
+        
